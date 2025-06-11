367329203821d8404a96c40d8d77d65a708e8b17
Glitch-dn <danielegambucci.lab@gmail.com>
2025-06-11 13:09:48 +0200

    req-category + offcanvas

---
+++ src/app/models/category.ts
@@ -0,0 +1,7 @@
+export type CategoryResponse = Category[]
+
+export interface Category {
+  slug: string
+  name: string
+  url: string
+}
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ProductsResponse } from '../models/product';
+import { CategoryResponse } from '../models/category';
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +13,20 @@ export class ProductsService {
   async getProducts(): Promise<ProductsResponse> {
     const res = await fetch('https://dummyjson.com/products');
     const data:ProductsResponse = await res.json();
-    // console.log(data);
     return data;
   }
+
+  async searchProducts(q: string){
+    const res = await fetch(`https://dummyjson.com/products/search?q=${encodeURIComponent(q)}`);
+    const data:ProductsResponse = await res.json();
+    return data;
+  }
+
+   async getCategories(): Promise<CategoryResponse>{
+    const res = await fetch('https://dummyjson.com/products/categories');
+    const data:CategoryResponse = await res.json();
+    return data;
+   }
 
   aggiornaStringaRicerca(r: string){
     console.log('Ricerca aggiornata:', r);
